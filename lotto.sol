503a93eef5b0ab03f78c1a15941cac80f940d618
Knowzzz <103582225+Knowzzz@users.noreply.github.com>
2022-05-23 22:36:21 +0200

    Add files via upload

--- lotto.sol
+++ lotto.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract Lotto is Ownable {
 
-contract Lotto is Ownable, ERC721 {
+constructor() {
 
-    constructor() () {
-
-    }
+}
 
     struct Lottos {
         uint maxPlayers;
@@ -18,35 +17,40 @@ contract Lotto is Ownable, ERC721 {
         string name;
         bool ended;
         uint price;
+        address[] players;
     }
+
     uint256 totalLottos;
     mapping(uint => Lottos) lottos;
     mapping(address => uint) balance;
     address payable ceo;
 
-    function createLotto(string memory _name, address _depositAddress, string memory _winObject, uint _maxPlayers, bool _ended, uint _price) public onlyOwner {
+function createLotto(string memory _name, address _depositAddress, string memory _winObject, uint _maxPlayers, bool _ended, uint _price) public onlyOwner {
         Lottos memory newLotto = Lottos(_maxPlayers, _winObject, _depositAddress, _name, _ended, _price);
         lottos[totalLottos] = newLotto;
         totalLottos++;
     }
+
   function enter(uint lottoId) public payable {
         Lottos memory entering = lottos[lottoId];
+        lottos[lottoId] = entering;
         require(entering.ended == false, "Loto is already finish");
         require(msg.value == entering.price, "Price not valid");
         require(msg.sender != address(0), "Your address must be different than address 0");
         bool isAlreadyRegister = false;
-        for (uint i=0; i<entering.length; i++) {
-            if (entering[i] == msg.sender) {
+        for (uint i=0; i<entering.players.length; i++) {
+            if (entering.players[i] == msg.sender) {
                 isAlreadyRegister = true;
             }
         }
         require(isAlreadyRegister == false);
-        entering.push(msg.sender);
+        entering.players.push(msg.sender);
         entering.depositAddress.transfer(msg.value);
+        
   }
 
   function random() private view returns (uint) {
-    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, lottos)));
+    return uint(keccak256(abi.encodePacked(block.difficulty, now, lottos)));
 }
 
   function win(uint lottoId) public onlyOwner {
@@ -55,7 +59,7 @@ contract Lotto is Ownable, ERC721 {
     ceo.transfer(entering.depositAddress);
     entering.ended = true;
     lottos[lottoId] = entering;
-}
+    }
 
     function nbrLotto() public view returns (uint) {
         return totalLottos;
@@ -66,13 +70,9 @@ contract Lotto is Ownable, ERC721 {
         ceo.transfer(entering.depositAddress);
     }
 
-    function checkLotto(uint i) public view returns (uint, string memory, address, string memory, bool, uint) {
-        return (lottos[i].maxPlayers, lottos[i].winObject, lottos[i].depositAddress, lottos[i].name, lottos[i].ended, lottos[i].price); 
+    function checkLotto(uint i) public view returns (uint, string memory, address, string memory, bool, uint, address) {
+        return (lottos[i].maxPlayers, lottos[i].winObject, lottos[i].depositAddress, lottos[i].name, lottos[i].ended, lottos[i].price, lottos[i].players); 
     }
     
 }
-
-
-
-
 
